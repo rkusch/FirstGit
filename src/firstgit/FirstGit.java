bfcb7df4ebc9076ac01155d867b33351a07b7c37
rkusch <rkusch@10.18.12.1>
2017-08-29 15:43:49 -0500

    Revert "added second comment to main"
    This reverts commit 603ea011e5c11359c3705e39f910dfec32a8d00a.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,7 +12,6 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Second comment");
     }
     
 }
